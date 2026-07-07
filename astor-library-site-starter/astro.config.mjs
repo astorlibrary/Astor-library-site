7af3993bc3487cfed507ea9e434b637f0316eb13
astorlibrary <haydnawood@gmail.com>
2026-07-07 10:09:33 +0100

    Delete astor-library-site-starter directory

--- astor-library-site-starter/astro.config.mjs
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://www.astorlibrary.co.uk'
-});
--- astor-library-site-starter/src/site.config.ts
+++
@@ -1,7 +0,0 @@
-export const siteConfig = {
-  name: 'Astor Library',
-  url: 'https://www.astorlibrary.co.uk',
-  description: 'Readable editions of classic texts and study resources for English and Media.',
-  email: 'hello@astorlibrary.co.uk',
-  tagline: 'Making classic literature easier to read, teach, study and appreciate.'
-};
